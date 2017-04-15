eae85d7a83ea8979ef4675296ae96091b7edf0b1
Greg Lee <gml2136@gmail.com>
2017-04-15 12:23:04 -0700

    random error that changing to a switch statement fixed, repeat random

--- index.js
+++ index.js
@@ -4,21 +4,18 @@ const app = express();
 const PORT = 8080;
 
 app.get("/:page?", function(req, res) {
-  if(req.params.page){
     switch(req.params.page){
       case "make":
         console.log(`Request for ${req.params.page}`);
-        res.sendFile('reservation.html', {root: __dirname});
+        res.sendFile('reservations.html', {root: __dirname});
         break;
       case "list":
         console.log(`Request for ${req.params.page}`);
         res.sendFile('tables.html', {root: __dirname});
         break;
+      default:
+        res.sendFile('index.html', {root: __dirname});
     }
-  } else {
-    console.log("Root Requested");
-    res.sendFile("index.html", {root: __dirname});
-  }
 });
 
 app.get("/api/:path?", function(req, res) {
